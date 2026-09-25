cfc126b745007fe90244809213e3fe42f8505c98
Lokesh-Ramineni <lokesh.ramineni1@gmail.com>
2026-09-25 18:55:24 +0530

    payloads

---
+++ Download/payloads.py
@@ -0,0 +1,19 @@
+def make_payload_1(csrf, sem_sub_id, authorized_id, timestamp):
+    return {
+        "_csrf": csrf,
+        "paramReturnId": "getCourseForCoursePage",
+        "semSubId": sem_sub_id,
+        "authorizedID": authorized_id,
+        "x": timestamp
+    }
+
+def make_payload_2(csrf, class_id,sem_sub_id, authorized_id,timestamp):
+    return {
+        "_csrf": csrf,
+        "classId": class_id,
+        "praType": "source",
+        "paramReturnId": "getSlotIdForCoursePage",
+        "semSubId": sem_sub_id,
+        "authorizedID": authorized_id,
+        "x": timestamp
+    }
